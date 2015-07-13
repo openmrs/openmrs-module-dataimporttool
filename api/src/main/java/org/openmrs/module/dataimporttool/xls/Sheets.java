ad5497f22dd60044fe899dee6511e84c85a339ad
Nyah Check <check.nyah@gmail.com>
2015-07-13 10:24:52 +0100

    Adding xls processing classes

---
+++ api/src/main/java/org/openmrs/module/dataimporttool/xls/Sheets.java
@@ -0,0 +1,79 @@
+package org.esaude.dmt.xls;
+
+/**
+ * A list of index mapped to the XLS input file
+ * @author Valério João
+ * @since 22-08-2014
+ *
+ */
+public class Sheets {
+	
+	public static final class TUPLE {
+		public static final String NAME = "TUPLE"; 
+		public static final int INDEX = 0;
+		public static final int ROW_START = 1;
+		public static final int ID = 0;
+		public static final int TERMINOLOGY = 1;
+		public static final int TABLE = 2;
+		public static final int PREDECESSOR = 3;
+		public static final int DESC = 4;
+	}
+	
+	public static final class MATCH_L_TO_R {
+		public static final String NAME = "MATCH_L_TO_R"; 
+		public static final int INDEX = 1;
+		public static final int ROW_START = 2;
+		public static final int TUPLE_ID = 0;
+		public static final int TERMINOLOGY = 1;
+		public static final int COLUMN_L = 2;
+		public static final int DATATYPE_L = 3;
+		public static final int SIZE_L = 4;
+		public static final int REQUIRED_L = 5;
+		public static final int PK = 6;
+		public static final int TABLE_R = 7;
+		public static final int COLUMN_R = 8;
+		public static final int DATATYPE_R = 9;
+		public static final int SIZE_R = 10;
+		public static final int REQUIRED_R = 11;
+		public static final int MATCH_ID = 12;
+		public static final int OBS = 13;
+		public static final int DEFAULT_VALUE = 14;
+		public static final int VALUE_MATCH = 15;
+		public static final int POOL = 16;
+	}
+	
+	public static final class REFERENCES {
+		public static final String NAME_L = "REFERENCES_L"; 
+		public static final int INDEX_L = 2;
+		public static final String NAME_R = "REFERENCES_R"; 
+		public static final int INDEX_R = 3;
+		public static final int ROW_START = 1;
+		public static final int ID = 0;
+		public static final int TUPLE_MATCH_ID = 1;
+		public static final int REFERENCE_TABLE = 2;
+		public static final int REFERENCE_COLUMN = 3;
+		public static final int REFERENCED_TABLE = 4;
+		public static final int REFERENCED_COLUMN = 5;
+		public static final int DATATYPE = 6;
+		public static final int SIZE = 7;
+		public static final int REFERENCED_VALUE = 8;
+		public static final int SEQUENCE = 9;
+		public static final int NAME_DESC = 10;	
+	}
+	
+	public static final class VALUE_MATCH {
+		public static final String NAME = "VALUE_MATCH"; 
+		public static final int INDEX = 4;
+		public static final int ROW_START = 2;
+		public static final int ID = 0;
+		public static final int GROUP_NAME = 1;
+		public static final int TABLE_L = 2;
+		public static final int COLUMN_L = 3;
+		public static final int VALUE_L = 4;
+		public static final int PK_L = 5;
+		public static final int TABLE_R = 6;
+		public static final int COLUMN_R = 7;
+		public static final int VALUE_R = 8;
+		public static final int PK_R = 9;
+	}
+}
---
+++ api/src/main/java/org/openmrs/module/dataimporttool/xls/XlsProcessor.java
@@ -0,0 +1,76 @@
+package org.esaude.dmt.xls;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import org.esaude.dmt.config.schema.Config;
+import org.esaude.dmt.util.ConfigReader;
+
+/**
+ * A tool that reads data from XLS files. It uses JXL API
+ * 
+ * @author Valério João
+ * @since 21-08-2014
+ *
+ */
+public class XlsProcessor {
+	private final Config config = ConfigReader.getInstance().getConfig();
+	private Workbook workbook;
+
+	/**
+	 * Default constructor
+	 */
+	public XlsProcessor() {
+		// get the matching file using config info
+		File inputWorkbook = new File(config.getMatchingInput().getLocation()
+				+ "/" + config.getMatchingInput().getFileName() + "."
+				+ config.getMatchingInput().getFormat());
+
+		try {
+			workbook = Workbook.getWorkbook(inputWorkbook);
+		} catch (BiffException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Parameterized constructor
+	 * 
+	 * @param workbook
+	 */
+	public XlsProcessor(Workbook workbook) {
+		this.workbook = workbook;
+	}
+
+	/**
+	 * Return a table value regardless the data type
+	 * 
+	 * @param SHEET
+	 *            the sheet of the value
+	 * @param COLLUMN
+	 *            the column of the value
+	 * @param ROW
+	 *            the row of the value
+	 * @return
+	 * @throws IOException
+	 */
+	public String process(final int SHEET, final int COLLUMN, final int ROW) {
+		Sheet sheet = workbook.getSheet(SHEET);
+
+		return sheet.getCell(COLLUMN, ROW).getContents();
+	}
+	
+	/**
+	 * Returns the number of rows of a certain sheet
+	 * @param SHEET
+	 * @return
+	 */
+	public int getSize(final int SHEET) {
+		return workbook.getSheet(SHEET).getRows();
+	}
+
+}
