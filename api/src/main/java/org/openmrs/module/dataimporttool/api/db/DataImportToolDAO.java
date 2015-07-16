b8315f242b954470bda0790dd4f54b14400dc4c7
Nyah Check <check.nyah@gmail.com>
2015-07-16 15:40:49 +0100

    Modified the DMT Data to match the OpenMRS DAO object

--- api/src/main/java/org/openmrs/module/dataimporttool/api/db/DataImportToolDAO.java
+++ api/src/main/java/org/openmrs/module/dataimporttool/api/db/DataImportToolDAO.java
@@ -15,12 +15,158 @@ package org.openmrs.module.dataimporttool.api.db;
 
 import org.openmrs.module.dataimporttool.api.DataImportToolService;
 
+import java.util.List;
+
+import org.openmrs.api.db.DAOException;
+
 /**
  *  Database methods for {@link DataImportToolService}.
+ * 
+ * This class is used to manipulate SQL-based databases using SQL native queries
+ *
  */
 public interface DataImportToolDAO {
-	
-	/*
-	 * Add DAO methods here
+
+	/**
+	 * Allows to commit the transaction from outside
+	 * 
+	 * @throws DA0Exception
 	 */
+	public void commit() throws DAOException;
+
+	/**
+	 * Allows to rollback the transaction from outside
+ 	 *
+	 * @throws DAOException
+	 */
+	public void rollback() throws DAOException;
+	
+	/**
+	 * Allows to save the point to rollback from outside
+	 *
+	 * @throws DAOException
+	 */
+	public void setSavePoint() throws DAOException;
+
+	/**
+	 * This method is to execute a query in the database to retrieve data in rows & columns
+	 *
+	 * @param query
+	 * @return list of list objects.
+	 * @throws DAOException
+	 */
+	public List<List<Object>> executeQuery(String query) throws DAOException;
+
+	/**
+	 * This method close the connection
+	 * 
+	 * @throws DAOException
+	 */
+	public void close() throws DAOException;
+
+	/**
+	 * This method returns the name of retrieved table columns in a certain
+	 * position
+	 * 
+	 * @param column
+	 * @return
+	 */
+	public String getColumnName(int column)throws DAOException;
+
+	/**
+	 * This method map the database specific datatype into java language
+	 * datatype
+	 * 
+	 * @param column
+	 * @return
+	 */
+	public Class getColumnClass(int column) throws DAOException;
+
+	/**
+	 * This method returns the total number of columns
+	 * 
+	 * @return
+	 */
+	public int getColumnCount();
+
+	/**
+	 * This method returns the total number of rows
+	 * 
+	 * @return
+	 */
+	public int getRowCount();
+
+	/**
+	 * This method returns a value in specified position
+	 * 
+	 * @param aRow
+	 * @param aColumn
+	 * @return
+	 */
+	public Object getValueAt(int aRow, int aColumn);
+
+	/**
+	 * This method returns all data retrieved from the select statement executed
+	 * 
+	 * @return
+	 */
+	public List<List<Object>> getRows();
+
+	/**
+	 * This method maps java datatype into database specific datatype
+	 * 
+	 * @param column
+	 * @param value
+	 * @return
+	 */
+	public String dbRepresentation(int column, Object value);
+
+	/**
+	 * This method execute query and return the PKs of affected records
+	 * 
+	 * @param query
+	 * @return result
+	 */
+	public List<List<Object>> executeUpdate(String query) throws DAOException;
+
+	/**
+	 * This method execute prepared statement that can affect multiple columns
+	 *
+	 * @param query
+	 * @param parameters
+	 * @return
+	 * @throws DAOException
+	 */
+	public int executePreparedStatement(String query, List<Object> parameters)
+			throws DAOException;
+
+	/**
+	 * This method execute prepared statement to that can affect single column
+	 * using id filtering
+	 *
+	 * @param query
+	 * @param value
+	 * @param target
+	 * @return
+	 * @throws DAOException
+	 */
+	public int executePreparedUpdate(String query, Object value, Object target)
+			throws DAOException;
+
+	/**
+	 * This method takes a value and returns its string representation in the database
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public String cast(Object value) throws DAOException;
+
+	/**
+	 * This method aggregates the result set in a java { @link List }
+	 *
+	 * @return list of list objects
+	 * @throws DAOException
+	 */
+	public List<List<Object>> constructRows() throws DAOException;
+
 }
