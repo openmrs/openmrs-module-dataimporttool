1b58d0a23e80811d4af35b8ec99f7c3b6e71bf25
Nyah Check <check.nyah@gmail.com>
2015-07-31 21:49:21 +0100

    Removed privilege check for UI

--- omod/src/main/java/org/openmrs/module/dataimporttool/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/dataimporttool/html/AdminList.java
@@ -26,7 +26,6 @@ import org.openmrs.module.web.extension.AdministrationSectionExt;
  */
 public class AdminList extends AdministrationSectionExt {
 	
-	public static final String DO_MIGRATION = "Start Migration";
 	/**
 	 * @see AdministrationSectionExt#getMediaType()
 	 */
@@ -46,11 +45,9 @@ public class AdminList extends AdministrationSectionExt {
 	 */
 	public Map<String, String> getLinks() {
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-
-		if (Context.hasPrivilege(DO_MIGRATION)) {
-			map.put("/module/dataimporttool/addMigrationSettings.form", "Start Migration");
-			map.put("/module/dataimporttool/help.form", "View Help");
-		}
+		
+		map.put("/module/dataimporttool/addMigrationSettings.form", "Start Migration");
+		map.put("/module/dataimporttool/help.form", "View Help");
 
 		return map;
 	}
