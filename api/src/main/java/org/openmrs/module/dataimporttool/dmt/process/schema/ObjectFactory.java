f6f2901254c3aca5219b8708a55f831cd09f4511
Nyah Check <check.nyah@gmail.com>
2015-08-17 10:50:34 +0100

    Fixes to Oject Factory class

--- api/src/main/java/org/openmrs/module/dataimporttool/dmt/process/schema/ObjectFactory.java
+++ api/src/main/java/org/openmrs/module/dataimporttool/dmt/process/schema/ObjectFactory.java
@@ -12,18 +12,16 @@
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 
+
 package org.openmrs.module.dataimporttool.dmt.process.schema;
 
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.annotation.XmlElementDecl;
 import javax.xml.bind.annotation.XmlRegistry;
-import javax.xml.namespace.QName;
 
 
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the org.openmrs.module.dataimporttool.matchingschema package. 
+ * generated in the org.openmrs.module.dataimporttool.dmt.process.schema package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -34,66 +32,23 @@ import javax.xml.namespace.QName;
  * provided in this class.
  * 
  */
-@SuppressWarnings("restriction")
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _Tuple_QNAME = new QName("http://esaude.org/matchingschema", "tuple");
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.openmrs.module.dataimporttool.matchingschema
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.openmrs.module.dataimporttool.dmt.process.schema
      * 
      */
     public ObjectFactory() {
     }
 
     /**
-     * Create an instance of {@link TupleType }
+     * Create an instance of {@link Process }
      * 
      */
-    public TupleType createTupleType() {
-        return new TupleType();
-    }
-
-    /**
-     * Create an instance of {@link ReferenceType }
-     * 
-     */
-    public ReferenceType createReferenceType() {
-        return new ReferenceType();
-    }
-
-    /**
-     * Create an instance of {@link ReferenceSideType }
-     * 
-     */
-    public ReferenceSideType createReferenceSideType() {
-        return new ReferenceSideType();
-    }
-
-    /**
-     * Create an instance of {@link MatchType }
-     * 
-     */
-    public MatchType createMatchType() {
-        return new MatchType();
-    }
-
-    /**
-     * Create an instance of {@link MatchSideType }
-     * 
-     */
-    public MatchSideType createMatchSideType() {
-        return new MatchSideType();
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link TupleType }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://esaude.org/matchingschema", name = "tuple")
-    public JAXBElement<TupleType> createTuple(TupleType value) {
-        return new JAXBElement<TupleType>(_Tuple_QNAME, TupleType.class, null, value);
+    public Process createProcess() {
+        return new Process();
     }
 
 }
